029383899a966946e9d25b34f0644d3fadd22f78
Richard Bos <richard.bos93@gmail.com>
2018-05-10 19:28:02 +0200

    Added Example Service

---
+++ src/ExampleService/ExampleManager.cs
@@ -0,0 +1,34 @@
+﻿using Pitstop.Infrastructure.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleService
+{
+    public class ExampleManager : IMessageHandlerCallback
+    {
+        IMessagePublisher _messagePublisher;
+        IMessageHandler _messageHandler;
+
+        public ExampleManager(IMessagePublisher messagePublisher, IMessageHandler messageHandler)
+        {
+            _messageHandler = messageHandler;
+        }
+
+        public void Start()
+        {
+            _messageHandler.Start(this);
+        }
+
+        public void Stop()
+        {
+            _messageHandler.Stop();
+        }
+
+        public Task<bool> HandleMessageAsync(MessageTypes messageType, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ExampleService/Program.cs
@@ -0,0 +1,59 @@
+﻿using ExampleService;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace Pitstop.ExampleService
+{
+    class Program
+    {
+        private static string _env;
+        public static IConfigurationRoot Config { get; private set; }
+
+        static Program()
+        {
+            _env = Environment.GetEnvironmentVariable("PITSTOP_ENVIRONMENT") ?? "Production";
+
+            Console.WriteLine($"Environment: {_env}");
+
+            Config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{_env}.json", optional: false)
+                .Build();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // get configuration
+            var configSection = Config.GetSection("RabbitMQ");
+            string host = configSection["Host"];
+            string userName = configSection["UserName"];
+            string password = configSection["Password"];
+
+            // start example manager
+            RabbitMQMessagePublisher messagePublisher = new RabbitMQMessagePublisher(host, userName, password, "Pitstop");
+            RabbitMQMessageHandler messageHandler = new RabbitMQMessageHandler(host, userName, password, "Pitstop", "Example", "");
+            ExampleManager manager = new ExampleManager(messagePublisher, messageHandler);
+            manager.Start();
+
+            if (_env == "Development")
+            {
+                Console.WriteLine("Example service started. Press any key to stop...");
+                Console.ReadKey(true);
+                //manager.Stop();
+            }
+            else
+            {
+                Console.WriteLine("Example service started.");
+                while (true)
+                {
+                    Thread.Sleep(10000);
+                }
+            }
+        }
+    }
+}
