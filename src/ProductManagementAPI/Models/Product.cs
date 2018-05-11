5a7d7e29e0f7483459496c44e1dffcf17c67c93c
Danny <kenwai2010@gmail.com>
2018-05-11 14:46:08 +0200

    Product Management Setup

---
+++ src/ProductManagementAPI/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductManagementAPI.Models
+{
+	public class Product
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public decimal Price { get; set; }
+
+		public string Type { get; set; }
+	}
+}
---
+++ src/ProductManagementAPI/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Pitstop.Infrastructure.Messaging;
+
+namespace ProductManagementAPI
+{
+    public class Startup
+    {
+	    public Startup(IHostingEnvironment env)
+	    {
+		    var builder = new ConfigurationBuilder()
+			    .SetBasePath(env.ContentRootPath)
+			    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+			    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+			    .AddEnvironmentVariables();
+		    Configuration = builder.Build();
+	    }
+
+		public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+	        // add messagepublisher classes
+	        var configSection = Configuration.GetSection("RabbitMQ");
+	        string host = configSection["Host"];
+	        string userName = configSection["UserName"];
+	        string password = configSection["Password"];
+	        services.AddTransient<IMessagePublisher>((sp) => new RabbitMQMessagePublisher(host, userName, password, "Pitstop"));
+
+			services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
