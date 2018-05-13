79de6e07bf445f6411c1de289b5126343af0160f
Danny <kenwai2010@gmail.com>
2018-05-13 23:13:42 +0200

    added swagger ui

--- src/ProductManagementAPI/Startup.cs
+++ src/ProductManagementAPI/Startup.cs
@@ -11,6 +11,7 @@ using ProductManagementAPI.Infrastructure.Database;
 using ProductManagementAPI.Infrastructure.Events;
 using ProductManagementAPI.Models;
 using ProductManagementAPI.Repositories;
+using Swashbuckle.AspNetCore.Swagger;
 using System;
 
 namespace ProductManagementAPI
@@ -52,7 +53,13 @@ namespace ProductManagementAPI
 	        services.AddTransient<ProductDbSeeder>();
 
 			services.AddMvc();
-        }
+
+			// Register the Swagger generator, defining one or more Swagger documents
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new Info { Title = "VehicleManagement API", Version = "v1" });
+			});
+		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ProductDbSeeder seeder)
@@ -64,7 +71,17 @@ namespace ProductManagementAPI
 
             app.UseMvc();
 
+
 			SetupAutoMapper();
+
+			// Enable middleware to serve generated Swagger as a JSON endpoint.
+			app.UseSwagger();
+
+			// Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint.
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "VehicleManagement API - v1");
+			});
 
 			seeder.Seed().Wait();
 		}
