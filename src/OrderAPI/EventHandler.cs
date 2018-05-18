ecee0470ad8564db8d222f7531ae1e36cab2a7f1
Hugo Zink <hugo.zink@hotmail.com>
2018-05-18 16:31:03 +0200

    Added migration for order API database.

--- src/OrderAPI/EventHandler.cs
+++ src/OrderAPI/EventHandler.cs
@@ -53,29 +53,6 @@ namespace OrderAPI.EventHandler
             return true; 
         }
 
-        private async Task<bool> HandleAsync(VehicleRegistered e)
-        {
-            Console.WriteLine($"Vehicle registered: License = {e.LicenseNumber}, Brand = {e.Brand}, Type = {e.Type}, Owner Id: {e.OwnerId}");
-
-            try
-            {
-                await _dbContext.Vehicles.AddAsync(new Vehicle
-                {
-                    LicenseNumber = e.LicenseNumber,
-                    Brand = e.Brand,
-                    Type = e.Type,
-                    OwnerId = e.OwnerId
-                });
-                await _dbContext.SaveChangesAsync();
-            }
-            catch(DbUpdateException)
-            {
-                Console.WriteLine($"Skipped adding vehicle with license number {e.LicenseNumber}.");
-            }
-
-            return true;
-        }
-
         private async Task<bool> HandleAsync(CustomerRegistered e)
         {
             Console.WriteLine($"Customer registered: Customer Id = {e.CustomerId}, Name = {e.Name}, Telephone Number = {e.TelephoneNumber}");
@@ -96,79 +73,6 @@ namespace OrderAPI.EventHandler
             }
 
             return true; 
-        }
-
-        private async Task<bool> HandleAsync(MaintenanceJobPlanned e)
-        {
-            Console.WriteLine($"Maintenance job planned: JobId = {e.JobId}, StartTime = {e.StartTime}, EndTime = {e.EndTime}, Customer = {e.CustomerInfo.Name}, Vehicle = {e.VehicleInfo.LicenseNumber}");
-
-            try
-            {
-                // determine customer
-                Customer customer = await _dbContext.Customers.FirstOrDefaultAsync(c => c.CustomerId == e.CustomerInfo.Id);
-                if (customer == null)
-                {
-                    customer = new Customer
-                    {
-                        CustomerId = e.CustomerInfo.Id,
-                        Name = e.CustomerInfo.Name,
-                        TelephoneNumber = e.CustomerInfo.TelephoneNumber
-                    };
-                }
-
-                // determine vehicle
-                Vehicle vehicle = await _dbContext.Vehicles.FirstOrDefaultAsync(v => v.LicenseNumber == e.VehicleInfo.LicenseNumber);
-                if (vehicle == null)
-                {
-                    vehicle = new Vehicle
-                    {
-                        LicenseNumber = e.VehicleInfo.LicenseNumber,
-                        Brand = e.VehicleInfo.Brand,
-                        Type = e.VehicleInfo.Type,
-                        OwnerId = customer.CustomerId
-                    };
-                }
-
-                // insert maintetancejob
-                await _dbContext.MaintenanceJobs.AddAsync(new MaintenanceJob
-                {
-                    Id = e.JobId,
-                    StartTime = e.StartTime,
-                    EndTime = e.EndTime,
-                    Customer = customer,
-                    Vehicle = vehicle,       
-                    WorkshopPlanningDate = e.StartTime.Date,
-                    Description = e.Description
-                });
-                await _dbContext.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
-            {
-                Console.WriteLine($"Skipped adding maintenance job with id {e.JobId}.");
-            }
-
-            return true;
-        }
-
-        private async Task<bool> HandleAsync(MaintenanceJobFinished e)
-        {
-            Console.WriteLine($"Maintenance job finished: JobId = {e.JobId}, ActualStartTime = {e.StartTime}, EndTime = {e.EndTime}");
-
-            try
-            {
-                // insert maintetancejob
-                var job = await _dbContext.MaintenanceJobs.FirstOrDefaultAsync(j => j.Id == e.JobId);
-                job.ActualStartTime = e.StartTime;
-                job.ActualEndTime = e.EndTime;
-                job.Notes = e.Notes;
-                await _dbContext.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
-            {
-                Console.WriteLine($"Skipped adding maintenance job with id {e.JobId}.");
-            }
-
-            return true;
         }
     }
 }
