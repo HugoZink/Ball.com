f1cbff5148b98e863ed79e98da37e01fdeb58b8f
Danny <kenwai2010@gmail.com>
2018-05-22 17:21:04 +0200

    Changed weights to decimal and added to events

--- src/ProductManagementAPI/Infrastructure/Events/NewProductAdded.cs
+++ src/ProductManagementAPI/Infrastructure/Events/NewProductAdded.cs
@@ -9,14 +9,16 @@ namespace ProductManagementAPI.Infrastructure.Events
 		public readonly string Id;
 		public readonly string Name;
 		public readonly decimal Price;
+		public readonly decimal WeightKg;
 		public readonly ProductType Type;
 
-		public NewProductAdded(Guid messageId, string id, string name, decimal price, ProductType type) : 
+		public NewProductAdded(Guid messageId, string id, string name, decimal price, decimal weightkg, ProductType type) : 
 			base(messageId, MessageTypes.NewProductAdded)
 		{
 			Id = id;
 			Name = name;
 			Price = price;
+			WeightKg = weightkg;
 			Type = type;
 		}
 	}
