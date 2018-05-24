b9593b927f92948d3c154024ce1f0d1a33a8b411
Richard Bos <richard.bos93@gmail.com>
2018-05-25 01:37:05 +0200

    Fixed OrderState (threw exception -> now nullable)

--- src/OrderAPI/Controllers/OrdersController.cs
+++ src/OrderAPI/Controllers/OrdersController.cs
@@ -16,157 +16,157 @@ namespace OrderAPI.Controllers
     [Route("api/[controller]")]
     public class OrdersController : Controller
     {
-		IMessagePublisher _messagePublisher;
-		OrderDbContext _dbContext;
+        IMessagePublisher _messagePublisher;
+        OrderDbContext _dbContext;
 
-		public OrdersController(OrderDbContext dbContext, IMessagePublisher messagePublisher)
-		{
-			_dbContext = dbContext;
-			_messagePublisher = messagePublisher;
-		}
+        public OrdersController(OrderDbContext dbContext, IMessagePublisher messagePublisher)
+        {
+            _dbContext = dbContext;
+            _messagePublisher = messagePublisher;
+        }
 
-		[HttpGet]
+        [HttpGet]
         public async Task<IActionResult> GetAllAsync()
         {
-			return Ok(await _dbContext.Orders.ToListAsync());
-		}
+            return Ok(await _dbContext.Orders.ToListAsync());
+        }
 
-		[HttpGet]
-		[Route("{orderId}", Name = "GetByOrderId")]
-		public async Task<IActionResult> GetByOrderId(string orderId)
+        [HttpGet]
+        [Route("{orderId}", Name = "GetByOrderId")]
+        public async Task<IActionResult> GetByOrderId(string orderId)
         {
-			var order = await _dbContext.Orders.FirstOrDefaultAsync(o => o.OrderId == orderId);
-			if(order == null)
-			{
-				return NotFound();
-			}
+            var order = await _dbContext.Orders.FirstOrDefaultAsync(o => o.OrderId == orderId);
+            if (order == null)
+            {
+                return NotFound();
+            }
 
-			return Ok(order);
+            return Ok(order);
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateOrder()
         {
-			var order = new Order();
-			order.AddStateChange(OrderState.PENDING);
+            var order = new Order();
+            order.AddStateChange(OrderState.PENDING);
 
-			_dbContext.Orders.Add(order);
-			await _dbContext.SaveChangesAsync();
+            _dbContext.Orders.Add(order);
+            await _dbContext.SaveChangesAsync();
 
-			return CreatedAtRoute("GetByOrderId", new { orderId = order.OrderId }, order);
+            return CreatedAtRoute("GetByOrderId", new { orderId = order.OrderId }, order);
         }
 
-		[HttpPost]
-		[Route("{orderId}", Name = "AddProductToOrder")]
-		public async Task<IActionResult> AddProductToOrder(string orderId, [FromBody] string productId, [FromBody] int quantity)
-		{
-			var order = await _dbContext.Orders.FirstOrDefaultAsync(o => o.OrderId == orderId);
-			if (order == null)
-			{
-				return NotFound();
-			}
+        [HttpPost]
+        [Route("{orderId}", Name = "AddProductToOrder")]
+        public async Task<IActionResult> AddProductToOrder(string orderId, [FromBody] string productId, [FromBody] int quantity)
+        {
+            var order = await _dbContext.Orders.FirstOrDefaultAsync(o => o.OrderId == orderId);
+            if (order == null)
+            {
+                return NotFound();
+            }
 
-			var product = await _dbContext.Products.FirstOrDefaultAsync(p => p.ProductId == productId);
-			if (product == null)
-			{
-				return NotFound();
-			}
+            var product = await _dbContext.Products.FirstOrDefaultAsync(p => p.ProductId == productId);
+            if (product == null)
+            {
+                return NotFound();
+            }
 
-			//Create link model
-			var orderProduct = new OrderProduct();
-			orderProduct.Order = order;
-			orderProduct.Product = product;
-			orderProduct.Quantity = quantity;
+            //Create link model
+            var orderProduct = new OrderProduct();
+            orderProduct.Order = order;
+            orderProduct.Product = product;
+            orderProduct.Quantity = quantity;
 
-			order.OrderProducts.Add(orderProduct);
-			await _dbContext.SaveChangesAsync();
+            order.OrderProducts.Add(orderProduct);
+            await _dbContext.SaveChangesAsync();
 
-			return AcceptedAtRoute("GetByOrderId", new { orderId = order.OrderId }, order);
-		}
+            return AcceptedAtRoute("GetByOrderId", new { orderId = order.OrderId }, order);
+        }
 
-		[HttpPost]
-		[Route("{orderId}/place", Name = "PlaceOrder")]
-		public async Task<IActionResult> PlaceOrder(string orderId, [FromBody] PlaceOrder orderCommand)
-		{
-			if(!ModelState.IsValid)
-			{
-				return BadRequest(ModelState);
-			}
+        [HttpPost]
+        [Route("{orderId}/place", Name = "PlaceOrder")]
+        public async Task<IActionResult> PlaceOrder(string orderId, [FromBody] PlaceOrder orderCommand)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
-			var order = await _dbContext.Orders.FirstOrDefaultAsync(o => o.OrderId == orderId);
-			if (order == null)
-			{
-				return NotFound();
-			}
+            var order = await _dbContext.Orders.FirstOrDefaultAsync(o => o.OrderId == orderId);
+            if (order == null)
+            {
+                return NotFound();
+            }
 
-			order.AfterPayment = orderCommand.AfterPayment;
+            order.AfterPayment = orderCommand.AfterPayment;
 
-			if(order.AfterPayment)
-			{
-				order.AddStateChange(OrderState.AWAITINGAFTERPAYMENT);
-			}
-			else
-			{
-				order.AddStateChange(OrderState.PAYMENTINPROGRESS);
-			}
+            if (order.AfterPayment)
+            {
+                order.AddStateChange(OrderState.AWAITINGAFTERPAYMENT);
+            }
+            else
+            {
+                order.AddStateChange(OrderState.PAYMENTINPROGRESS);
+            }
 
-			await _dbContext.SaveChangesAsync();
+            await _dbContext.SaveChangesAsync();
 
-			OrderPlaced e = Mapper.Map<OrderPlaced>(order);
-			await _messagePublisher.PublishMessageAsync(e.MessageType, e, "");
+            OrderPlaced e = Mapper.Map<OrderPlaced>(order);
+            await _messagePublisher.PublishMessageAsync(e.MessageType, e, "");
 
-			return AcceptedAtRoute("GetByOrderId", new { orderId = order.OrderId }, order);
-		}
+            return AcceptedAtRoute("GetByOrderId", new { orderId = order.OrderId }, order);
+        }
 
-		[HttpDelete]
-		[Route("{orderId}/{productId}", Name = "RemoveProductFromOrder")]
-		public async Task<IActionResult> RemoveProductFromOrder(string orderId, string productId)
-		{
-			var order = await _dbContext.Orders.FirstOrDefaultAsync(o => o.OrderId == orderId);
-			if (order == null)
-			{
-				return NotFound("Could not find order");
-			}
+        [HttpDelete]
+        [Route("{orderId}/{productId}", Name = "RemoveProductFromOrder")]
+        public async Task<IActionResult> RemoveProductFromOrder(string orderId, string productId)
+        {
+            var order = await _dbContext.Orders.FirstOrDefaultAsync(o => o.OrderId == orderId);
+            if (order == null)
+            {
+                return NotFound("Could not find order");
+            }
 
-			var product = await _dbContext.Products.FirstOrDefaultAsync(p => p.ProductId == productId);
-			if (product == null)
-			{
-				return NotFound("Could not find product");
-			}
+            var product = await _dbContext.Products.FirstOrDefaultAsync(p => p.ProductId == productId);
+            if (product == null)
+            {
+                return NotFound("Could not find product");
+            }
 
-			//Find link model
-			var orderProduct = order.OrderProducts.FirstOrDefault(op => op.OrderId == order.OrderId && op.ProductId == op.ProductId);
-			if(orderProduct == null)
-			{
-				return NotFound("This product has not been added to this order.");
-			}
+            //Find link model
+            var orderProduct = order.OrderProducts.FirstOrDefault(op => op.OrderId == order.OrderId && op.ProductId == op.ProductId);
+            if (orderProduct == null)
+            {
+                return NotFound("This product has not been added to this order.");
+            }
 
-			order.OrderProducts.Remove(orderProduct);
+            order.OrderProducts.Remove(orderProduct);
 
-			await _dbContext.SaveChangesAsync();
+            await _dbContext.SaveChangesAsync();
 
-			return AcceptedAtRoute("GetByOrderId", new { orderId = order.OrderId }, order);
-		}
+            return AcceptedAtRoute("GetByOrderId", new { orderId = order.OrderId }, order);
+        }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(string orderId)
         {
-			var order = await _dbContext.Orders.FirstOrDefaultAsync(o => o.OrderId == orderId);
-			if (order == null)
-			{
-				return NotFound();
-			}
+            var order = await _dbContext.Orders.FirstOrDefaultAsync(o => o.OrderId == orderId);
+            if (order == null)
+            {
+                return NotFound();
+            }
 
-			if(order.CurrentState != OrderState.PENDING)
-			{
-				return BadRequest("Cannot delete a non-pending order.");
-			}
+            if (order.CurrentState != OrderState.PENDING)
+            {
+                return BadRequest("Cannot delete a non-pending order.");
+            }
 
-			_dbContext.Orders.Remove(order);
+            _dbContext.Orders.Remove(order);
 
-			await _dbContext.SaveChangesAsync();
+            await _dbContext.SaveChangesAsync();
 
-			return Accepted();
-		}
+            return Accepted();
+        }
     }
 }
--- src/OrderAPI/Model/Order.cs
+++ src/OrderAPI/Model/Order.cs
@@ -8,86 +8,86 @@ namespace OrderAPI.Model
 {
     public class Order
     {
-		public string OrderId { get; set; }
+        public string OrderId { get; set; }
 
-		public Customer Customer { get; set; }
+        public Customer Customer { get; set; }
 
-		public DateTime DateTime { get; set; }
+        public DateTime DateTime { get; set; }
 
-		public string TrackingCode { get; set; }
+        public string TrackingCode { get; set; }
 
-		public List<OrderStateChange> StateChanges { get; set; }
+        public List<OrderStateChange> StateChanges { get; set; }
 
-		public List<OrderProduct> OrderProducts { get; set; }
+        public List<OrderProduct> OrderProducts { get; set; }
 
-		public bool AfterPayment { get; set; }
+        public bool AfterPayment { get; set; }
 
-		[NotMapped]
-		public IEnumerable<Product> Products
-		{
-			get
-			{
-				var products = new List<Product>();
-				foreach(OrderProduct op in OrderProducts)
-				{
-					products.Add(op.Product);
-				}
+        [NotMapped]
+        public IEnumerable<Product> Products
+        {
+            get
+            {
+                var products = new List<Product>();
+                foreach (OrderProduct op in OrderProducts)
+                {
+                    products.Add(op.Product);
+                }
 
-				return products;
-			}
-		}
+                return products;
+            }
+        }
 
-		[NotMapped]
-		public string CurrentState
-		{
-			get
-			{
-				return StateChanges.OrderBy(s => s.Version).Last().State;
-			}
-		}
+        [NotMapped]
+        public string CurrentState
+        {
+            get
+            {
+                return StateChanges.OrderBy(s => s.Version).LastOrDefault().State;
+            }
+        }
 
-		[NotMapped]
-		public OrderStateChange LastStateChange
-		{
-			get
-			{
-				return StateChanges.OrderBy(s => s.Version).Last();
-			}
-		}
+        [NotMapped]
+        public OrderStateChange LastStateChange
+        {
+            get
+            {
+                return StateChanges.OrderBy(s => s.Version).LastOrDefault();
+            }
+        }
 
-		public Order()
-		{
-			this.OrderProducts = new List<OrderProduct>();
-			this.StateChanges = new List<OrderStateChange>();
-		}
+        public Order()
+        {
+            this.OrderProducts = new List<OrderProduct>();
+            this.StateChanges = new List<OrderStateChange>();
+        }
 
-		public void AddStateChange(string newState)
-		{
-			int newVersion;
-			var lastState = LastStateChange;
-			if(lastState == null)
-			{
-				newVersion = 1;
-			}
-			else
-			{
-				newVersion = lastState.Version + 1;
-			}
+        public void AddStateChange(string newState)
+        {
+            int newVersion;
+            var lastState = LastStateChange;
+            if (lastState == null)
+            {
+                newVersion = 1;
+            }
+            else
+            {
+                newVersion = lastState.Version + 1;
+            }
 
-			this.StateChanges.Add(new OrderStateChange()
-			{
-				State = newState,
-				Version = newVersion
-			});
-		}
+            this.StateChanges.Add(new OrderStateChange()
+            {
+                State = newState,
+                Version = newVersion
+            });
+        }
     }
 
-	public static class OrderState
-	{
-		public const string PENDING = "Pending";
-		public const string PAYMENTINPROGRESS = "Payment In Progress";
-		public const string PAYMENTCOMPLETE = "PaymentComplete";
-		public const string AWAITINGAFTERPAYMENT = "AwaitingAfterPayment";
-		public const string CLOSED = "Closed";
-	}
+    public static class OrderState
+    {
+        public const string PENDING = "Pending";
+        public const string PAYMENTINPROGRESS = "Payment In Progress";
+        public const string PAYMENTCOMPLETE = "PaymentComplete";
+        public const string AWAITINGAFTERPAYMENT = "AwaitingAfterPayment";
+        public const string CLOSED = "Closed";
+    }
 }
